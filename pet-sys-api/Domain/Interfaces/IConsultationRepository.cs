bee8dac95639a40a409c861c67ce917173497d39
Lucho74 <luchocilenti@gmail.com>
2026-07-30 19:31:00 -0300

    feat: add generic and entity-specific repository interfaces

---
+++ pet-sys-api/Domain/Interfaces/IConsultationRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IConsultationRepository : IBaseRepository<Consultation>
+    {
+        Task<IEnumerable<Consultation>> GetByVeterinarianIdAsync(string veterinarianId);
+        Task<IEnumerable<Consultation>> GetByStatusAsync(StatusConsultation status);
+    }
+}
