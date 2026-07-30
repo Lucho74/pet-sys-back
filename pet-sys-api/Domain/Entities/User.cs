5adacb832bbbd2fa87a173da6f72239be94d7d6a
TomasNanni <tomasnanni18@gmail.com>
2026-07-30 18:31:43 -0300

    fix: adjust User, Pet and Consultation entities

--- pet-sys-api/Domain/Entities/User.cs
+++ pet-sys-api/Domain/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -18,6 +19,7 @@ namespace Domain.Entities
         public string FullName { get; set; }
         [Required]
         [StringLength(100, MinimumLength = 6)]
+        [PasswordPropertyText]
         public string Password { get; set; }
         [Required]
         [EmailAddress]
