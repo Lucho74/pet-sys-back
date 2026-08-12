170c4df65fae5633e96a64018c69e95dad826a14
tomasNotebook <tomasnanni18@gmail.com>
2026-08-12 18:03:30 -0300

    fix: use Microsoft.EntityFrameworkCore instead of stray EF6 namespace in repositories
    BaseRepository, UserRepository, PetRepository and ConsultationRepository
    imported System.Data.Entity (EF6) instead of Microsoft.EntityFrameworkCore.
    It compiled because both namespaces expose same-shaped async LINQ
    extensions, but at runtime EF6's operators require IDbAsyncQueryProvider,
    which EF Core doesn't implement, so GetByIdAsync/GetAllAsync/FindAsync/
    ExistsAsync threw InvalidOperationException against any real EF Core
    provider.

--- pet-sys-api/Infrastructure/Repositories/ConsultationRepository.cs
+++ pet-sys-api/Infrastructure/Repositories/ConsultationRepository.cs
@@ -3,7 +3,7 @@ using Domain.Interfaces;
 using Infrastructure.Context;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
